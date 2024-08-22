a4a6177759c8692106963b9fc1b108450ea89583
tobsdhen <129931739+tobsdhen@users.noreply.github.com>
2024-08-21 19:36:33 -1100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php 
+$host="localhost";
+$user="root";
+$pass="";
+$db="login";
+
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error){
+    echo "Failed to connect to DB".$conn->connect_error;
+}
+?>
